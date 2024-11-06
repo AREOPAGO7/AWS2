6437e9eba536c9151d53d8b3379f0b208afa01ca
ANASS KALKHI <anaskalkhi123@gmail.com>
2024-11-06 22:32:07 +0100

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px', marginLeft:'500px' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px', marginLeft:'400px' }}>
       <div style={{ textAlign: 'center', maxWidth: '400px', width: '100%' }}>
         <h1>Todo List</h1>
         <input
